6b18733148a96078326ba741a6a744dffc237707
ankitrawat0097 <ankitrawat2099@gmail.com>
2025-12-28 20:42:59 +0530

    [ankit rawat] Solved.leetcode problem solved

---
+++ core-csharp-practice/leet-code-codebase/MaxConsecutiveOnes.cs
@@ -0,0 +1,17 @@
+public class Solution {
+    public int FindMaxConsecutiveOnes(int[] nums) {
+        int count=0;
+        int max=0;
+        for(int i=0;i<nums.Length;i++){
+            if(nums[i]==0){
+                max=Math.Max(max,count);
+                count=0;
+            }
+            else{
+                count++;
+                max=Math.Max(max,count);
+            }
+        }
+        return max;
+    }
+}
