28776ed12afb620a59817d9eed89c1de2cf9040d
ankitrawat0097 <ankitrawat2099@gmail.com>
2026-01-21 15:06:43 +0530

    [ankit rawat] Solved.scenario based problem solved

---
+++ collections-csharp-practice/scenario-based/smart-checkout/InventoryService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BridgeLabzTraining.collections_csharp_practice.scenario_based.smart_checkout
+{
+        public class InventoryService
+        {
+            private Dictionary<string, Item> items = new Dictionary<string, Item>(StringComparer.OrdinalIgnoreCase);
+        //method to add item
+            public void AddItem(Item item)
+            {
+                items[item.Name] = item;
+            }
+
+            public Item GetItem(string name)
+            {
+                return items.ContainsKey(name) ? items[name] : null;
+            }
+        //method to display inventory
+            public void DisplayInventory()
+            {
+                Console.WriteLine("\n--- Inventory ---");
+                foreach (var item in items.Values)
+                    Console.WriteLine(item);
+            }
+        }
+    }
+
+
