43a6b9b6f9377d68ca40aacbe5fd0545dfd42b77
ankitrawat0097 <ankitrawat2099@gmail.com>
2026-01-21 15:40:47 +0530

    [ankit rawat] Solved.leetcode problem solved

---
+++ datastructure-csharp-practice/leet-code-codebase/PalindromeLinkedList.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsPalindrome(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        ListNode mid=slow;
+        ListNode prev=null;
+        ListNode curr=slow;
+        ListNode next;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        ListNode right=prev;
+        ListNode left=head;
+        while(right!=null){
+            if(right.val!=left.val){
+                return false;
+            }
+            left=left.next;
+            right=right.next;
+        }
+        return true;
+    }
+}
